452f54a88c1402b5e22d3c3653a35ae65a9d94a1
Helios Ciancio <eshiol@users.noreply.github.com>
2017-11-07 10:56:07 +0100

    # report

--- components/com_buttons/models/category.php
+++ components/com_buttons/models/category.php
@@ -1,10 +1,7 @@
 <?php
 /**
- * @version		3.5.11 components/com_buttons/models/category.php
- * 
  * @package		Buttons
  * @subpackage	com_buttons
- * @since		3.4
  *
  * @author		Helios Ciancio <info@eshiol.it>
  * @link		http://www.eshiol.it
@@ -23,6 +20,8 @@ use Joomla\Registry\Registry;
 
 /**
  * Buttons Component Button Model
+ * @version		3.5.12
+ * @since		3.4
  */
 class ButtonsModelCategory extends JModelList
 {
@@ -156,7 +155,7 @@ class ButtonsModelCategory extends JModelList
 		}
 
 		// Join over the users for the author and modified_by names.
-		$query->select("CASE WHEN a.created_by_alias > ' ' THEN a.created_by_alias ELSE ua.name END AS author")
+		$query->select("CASE WHEN a.created_by_alias > '' THEN a.created_by_alias ELSE ua.name END AS author")
 			->select("ua.email AS author_email")
 			->join('LEFT', '#__users AS ua ON ua.id = a.created_by')
 			->join('LEFT', '#__users AS uam ON uam.id = a.modified_by');
